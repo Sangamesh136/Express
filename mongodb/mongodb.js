98635d6f92bbc4529dbbd2e0a87996cf8ed40a14
Sangamesh136 <sangameshvu136@gmail.com>
2026-09-10 14:59:25 +0530

    added gitignore

--- mongodb/mongodb.js
+++ mongodb/mongodb.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 mongoose.connect("mongodb+srv://sangameshvu136_db_user:E7LBjLf0AZV1a6MQ@cluster0.dfsxs3z.mongodb.net/?appName=Cluster0");
 
 const User = mongoose.model('Users',{ name:String, email:String, password:String });
